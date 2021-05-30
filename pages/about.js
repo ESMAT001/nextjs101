f185b0e533eb8b34be3bb850809fa20fa7660b3b
ESMAT001 <esmat10.niazi@gmail.com>
2021-05-30 11:58:04 +0430

    tailwind added with the first route

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+
+
+function about() {
+    return (
+        <div className="text-red-400 font-bold text-center capitalize mt-10 text-xl">
+            this is about!
+        </div>
+    )
+}
+
+export default about
